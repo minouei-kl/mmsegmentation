aa281e1e7501e7dcdb5e6d1c5cf92ab2e58a0152
minouei <mohammad.minooee@gmail.com>
2021-04-14 12:17:28 +0200

    danet

---
+++ da18.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install mmcv-full==1.2.5
+pip install git+file:///netscratch/minouei/report/mmsegmentation
+python -u tools/train.py configs/publay/danet_r50-d8_512x512_ver3.py  --launcher=slurm
