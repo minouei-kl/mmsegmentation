4be14011477e7beadceead96b8726ea89a6236a1
minouei <mohammad.minooee@gmail.com>
2021-06-08 12:11:36 +0200

    cnet

--- crun.sh
+++ crun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install mmcv-full==1.2.5
 pip install git+file:///netscratch/minouei/report/mmsegmentation
-python -u tools/train.py configs/ccnet/ccnet_r50-d8_512x512_80k_ade20k.py  --launcher=slurm
+python -u tools/train.py configs/publay/ccnet_r50-d8_512x512_80k_ver5.py  --launcher=slurm
 
